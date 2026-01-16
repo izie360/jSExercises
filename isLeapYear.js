448be359944e685110998f8983cb3200adb473d2
Izie DaCosta <idc93849@gmail.com>
2026-01-16 13:36:36 +0000

    Create isLeapYear.js

---
+++ isLeapYear.js
@@ -0,0 +1,19 @@
+let year=1900;
+
+function isLeapYear(year){
+
+let hCheck = (year%100===0)&&!(year%400===0);
+
+if(hCheck){
+  return year + " is not a leap year.";
+}else if(year%4===0){
+  return year + " is a leap year.";
+}else{
+  return year + " is not a leap year";
+}
+
+
+}
+
+let result = isLeapYear(year);
+console.log(result);
