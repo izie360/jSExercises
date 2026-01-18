2cc7da10e2a784e6f971eac4ea05d31bcce1133d
Izie DaCosta <idc93849@gmail.com>
2026-01-18 21:46:55 +0000

    Update pickALunch.js

--- pickALunch.js
+++ pickALunch.js
@@ -17,7 +17,7 @@ funcion addLunchtoStart(arr,str){
 function removeLastLunch(arr){
     if(arr.length ===0){
         console.log(`No lunches to remove.`);
-
+        return arr;
 
     }else{
 
@@ -26,3 +26,43 @@ function removeLastLunch(arr){
         return arr;
 
     }
+
+}
+
+function removeFirstLunch(arr){
+
+    if(arr.length===0){
+        console.log(`No lunches to remove.`);
+        return arr;
+    }else{
+        let rItem = arr.shift();
+        console.log(`${rItem} removed from the start of the lunch menu.`);
+        return arr;
+    }
+}
+
+function getRandomLunch(arr){
+
+    if(arr.length===0){
+        console.log(`No lunches available.`);
+        //return arr;
+    }else{
+        let randomN = Math.floor(Math.random()*arr.length);
+        let randomI = arr[randomN];
+        console.log(`Randomly selected lunch: ${randomI}`);
+    }
+
+}
+
+function showLunchMenu(arr){
+    if(arr.length===0){
+        console.log(`The menue is empty.`);
+        //return arr;
+    }else{
+        let str = arr.join( ,);
+        let displayText = "Menu items: "+ str;
+        console.log(displayText);
+
+    }
+
+}
