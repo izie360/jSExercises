1940195422ec6a2b2cbf7ed41f46ba14b201278d
Izie DaCosta <idc93849@gmail.com>
2026-01-30 09:13:01 +0000

    Update rCollection.js

--- rCollection.js
+++ rCollection.js
@@ -20,16 +20,47 @@ const recordCollection = {
 
 function updateRecords(records,id,prop,value){
   /*If value is an empty string, delete the given prop property from the album.*/
-  if(value=""){
+  if(value===""){
     delete records[id][prop];
   }
-  /*If prop isn't tracks and value isn't an empty string, assign the value to that album's prop.  */
+  /*3 If prop isn't tracks and value isn't an empty string, assign the value to that album's prop.  */
 
-if(prop==="tracks"&&value!==""){
+if(prop!=="tracks"&&value!==""){
   records[id][prop]=value;
 }
-/* If prop is tracks and value isn't an empty string, but the album doesn't have a tracks property, create an empty array and add value to it. */
+/*4. If prop is tracks and value isn't an empty string, but the album doesn't have a tracks property, create an empty array and add value to it. */
 
+if(prop==="tracks"&&value!==""){
+  let propChecker = prop in records[id];
+  if(!propChecker){
+    records[id][prop]=[value];
+  
+  }else{
+    /*5 If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array. */
+    records[id][prop].push(value);
 
-
+  }
 }
+return records;
+}
+
+/* You are creating a function that aids in the maintenance of a musical album collection. The collection is organized as an object that contains multiple albums which are also objects. Each album is represented in the collection with a unique id as the property name. Within each album object, there are various properties describing information about the album. Not all albums have complete information.
+
+The updateRecords function takes 4 arguments represented by the following function parameters:
+
+records - an object containing several individual albums
+id - a number representing a specific album in the records object
+prop - a string representing the name of the album’s property to update
+value - a string containing the information used to update the album’s property
+Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+User Stories:
+
+Your function must always return the entire records object.
+If value is an empty string, delete the given prop property from the album.
+If prop isn't tracks and value isn't an empty string, assign the value to that album's prop.
+If prop is tracks and value isn't an empty string, but the album doesn't have a tracks property, create an empty array and add value to it.
+If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
+Note: A copy of the recordCollection object is used for the tests. Your function should not directly refer to the recordCollection object, only the function parameter.
+
+  */
