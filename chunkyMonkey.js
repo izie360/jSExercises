83bc7c874eae475d028674b0d9425bf3fef27299
Izie DaCosta <idc93849@gmail.com>
2026-02-04 10:50:59 +0000

    Update chunkyMonkey.js

--- chunkyMonkey.js
+++ chunkyMonkey.js
@@ -1,25 +1,33 @@
+/* Implement the Chunky Monkey Algorithm
+Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+User Stories:
+
+Write a function named chunkArrayInGroups that takes an array as first argument and a number as second argument. 
+The function should split the array into smaller arrays of length equal 
+to the second argument and returns them as a two-dimensional array.   */
+
 function chunkArrayInGroups(arr,num){
 
   let twoDimArray=[];
 
-  let numStep=Math.ceil(arr.length/num);
-
-  
+ let numStep=Math.ceil(arr.length/num);
+ 
 
   let count=0;
 
   for(let i=0;i<numStep;i++){
 
     //let nArray;
-   if(count<arr.length){
-    
-      for(let j=0;j<num;j++){
+   
+     twoDimArray[i]=[];
+      for(let j=0;j<num&&count<arr.length;j++){
 
         //nArray[j]=arr[count];
         twoDimArray[i][j]= arr[count];
         count++;
       
-    }
+    
    }
     
 
