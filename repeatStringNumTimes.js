4dd6976bf503ed9ca77c7b25bf74157c11013829
Izie DaCosta <idc93849@gmail.com>
2026-02-05 07:58:45 +0000

    Update repeatStringNumTimes.js

--- repeatStringNumTimes.js
+++ repeatStringNumTimes.js
@@ -1,7 +1,4 @@
-/*    */
-
-
-Build a String Repeating Function
+/* Build a String Repeating Function
 In this lab, you will create a function that repeats a given string a specific number of times. For the purpose of this lab, do not use the built-in .repeat() method.
 
 Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
@@ -10,7 +7,10 @@ User Stories:
 
 You should create a function named repeatStringNumTimes that takes two parameters: a string and a number.
 The function should return the string repeated the specified number of times.
-If the number is less than or equal to zero, the function should return an empty string.
+If the number is less than or equal to zero, the function should return an empty string.   */
+
+
+
 function repeatStringNumTimes(str, num){
 
   if(num<=0){
